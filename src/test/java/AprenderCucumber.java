5068d9009f9045743fbbaf2972a58c3d65d17992
Gustavo Gois <gustavo.gois@gmail.com>
2020-01-19 19:51:37 +0000

    Cucumber Options (camel case). Métodos regerados em camel case, relates #2

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -5,17 +5,17 @@ import io.cucumber.java.pt.Quando;
 public class AprenderCucumber {
 
     @Dado("que criei o arquivo corretamente")
-    public void que_criei_o_arquivo_corretamente() {
+    public void queCrieiOArquivoCorretamente() {
         // Write code here that turns the phrase above into concrete actions
     }
 
     @Quando("executá-lo")
-    public void executá_lo() {
+    public void executáLo() {
         // Write code here that turns the phrase above into concrete actions
     }
 
     @Então("a especificação deve finalizar com sucesso")
-    public void a_especificação_deve_finalizar_com_sucesso() {
+    public void aEspecificaçãoDeveFinalizarComSucesso() {
         // Write code here that turns the phrase above into concrete actions
     }
 }
--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,6 +1,8 @@
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(snippets = CucumberOptions.SnippetType.CAMELCASE)
 public class Runner {
 }
