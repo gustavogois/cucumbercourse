33dfdf5af7c9a4410d5429dfd4a05f09fe1d9b58
Gustavo Gois <gustavo.gois@gmail.com>
2020-01-19 23:46:23 +0000

    50%, relates #5

--- src/test/java/steps/InserirContasSteps.java
+++ src/test/java/steps/InserirContasSteps.java
@@ -3,68 +3,66 @@ package steps;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.Assert.assertEquals;
+
 public class InserirContasSteps {
+
+    ChromeDriver chromeDriver;
+
     @Dado("que estou acessando a aplicação")
     public void queEstouAcessandoAAplicação() {
         System.setProperty("webdriver.chrome.driver", "/Users/gustavogois/projetos/cucumbercourse/src/test/resources/drivers/chromedriver");
-        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver = new ChromeDriver();
         chromeDriver.get("https://srbarriga.herokuapp.com");
     }
 
     @Quando("informo o usuário {string}")
-    public void informoOUsuário(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void informoOUsuário(String user) {
+        chromeDriver.findElement(By.id("email")).sendKeys(user);
     }
 
     @Quando("a senha {string}")
-    public void aSenha(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void aSenha(String password) {
+        chromeDriver.findElement(By.name("senha")).sendKeys(password);
     }
 
     @Quando("seleciono entrar")
     public void selecionoEntrar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        chromeDriver.findElement(By.tagName("button")).click();
     }
 
     @Então("visualizo a página inicial")
     public void visualizoAPáginaInicial() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        String text = chromeDriver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
+        assertEquals("Bem vindo, Gustavo!", text);
     }
 
     @Quando("seleciono Contas")
     public void selecionoContas() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
     }
 
     @Quando("seleciono Adicionar")
     public void selecionoAdicionar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
     }
 
     @Quando("informo a conta {string}")
     public void informoAConta(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
     }
 
     @Quando("seleciono Salvar")
     public void selecionoSalvar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
     }
 
     @Então("a conta é inserida com sucesso")
     public void aContaÉInseridaComSucesso() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
     }
 
 }
