fb51cd250c77fdf3138167b446a9f250e4439b5d
Gustavo Gois <gustavo.gois@gmail.com>
2020-01-19 19:01:20 +0000

    Criando o Runner. Foi preciso criar o test resources e mover o feature para lá, pois o arquivo feature não estava sendo encontrado, relates #2

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,24 +1,21 @@
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
 
 public class AprenderCucumber {
 
-    @Given("que criei o arquivo corretamente")
+    @Dado("que criei o arquivo corretamente")
     public void que_criei_o_arquivo_corretamente() {
         // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
     }
 
-    @When("executá-lo")
+    @Quando("executá-lo")
     public void executá_lo() {
         // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
     }
 
-    @Then("a especificação deve finalizar com sucesso")
+    @Então("a especificação deve finalizar com sucesso")
     public void a_especificação_deve_finalizar_com_sucesso() {
         // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
     }
 }
